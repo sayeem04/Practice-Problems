63840621935ac19cdafe6eacecf424f58a2604ac
SAYEEM <sayeem178@gmail.com>
2026-08-24 23:39:34 +0600

    Add files via upload

---
+++ DeleteDuplicateElements.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 3, 4,};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int newArr[100];
+    int newSize = 0;
+    for (int i = 0; i < size; i++) {
+        int isDuplicate = 0;
+        for (int j = 0; j < newSize; j++) {
+            if (arr[i] == newArr[j]) {
+                isDuplicate = 1;
+                break;
+            }
+        }
+
+        if (!isDuplicate) {
+            newArr[newSize] = arr[i];
+            newSize++;
+        }
+    }
+    cout << "Final Output : " << endl;
+    int i = 0;
+    while (i < newSize) {
+        cout << newArr[i] << " ";
+        i++;
+    }
+}
